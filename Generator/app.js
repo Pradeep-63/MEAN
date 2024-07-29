09dcbc538760edede85f4c1ac810afb7f6bb7b37
pradeep-63 <pradeepjana2222@gmail.com>
2024-07-29 10:35:17 +0530

    add

--- Generator/app.js
+++ Generator/app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // app.use(route);
-app.use(singnup)
+app.use("/api/v1",singnup)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
